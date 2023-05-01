1be32d5a97a7f50da5933e3ac4dfb1f1348d6efb
Tristan De Bartolo <tristano.debartolo@almaviacx.com>
2023-05-01 21:54:49 +0200

    [Preprocess] add class NodePreprocess

---
+++ src/Preprocess/NodePreprocess.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+namespace Drupal\cxdx_pretreatment\Preprocess;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\path_alias\AliasManagerInterface;
+use Drupal\node\NodeInterface;
+use Drupal\cxdx_pretreatment\Service\CxdxPretreatmentService;
+
+/**
+ * Handle all node preprocesses.
+ *
+ *@see template_preprocess_node
+ */
+final class NodePreprocess extends PreprocessBase {
+
+  /**
+   * The alias manager.
+   *
+   * @var \Drupal\path_alias\AliasManagerInterface
+   */
+  protected AliasManagerInterface $aliasManager;
+
+  /**
+   * LocalBlocStorage service
+   *
+   * @var \Drupal\cxdx_pretreatment\Service\CxdxPretreatmentService
+   */
+  private CxdxPretreatmentService $cps;
+
+  /**
+   * @param \Drupal\path_alias\AliasManagerInterface $alias_manager
+   *    The alias manager.
+   * @param \Drupal\cxdx_pretreatment\Service\CxdxPretreatmentService $cps
+   *    The CxdxPreprocessService service
+   */
+  public function __construct(AliasManagerInterface $alias_manager, CxdxPretreatmentService $cps) {
+    $this->aliasManager = $alias_manager;
+    $this->cps = $cps;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public static function create(ContainerInterface $container): NodePreprocess {
+    return new static(
+      $container->get('path_alias.manager'),
+      $container->get('cxdx_pretreatment.builder')
+    );
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function main(array &$variables): void {
+    // Get current node
+    $node = $variables['node'];
+    // Stire the id and the bundle
+    $nid = $node->id();
+    $bundle = $node->bundle();
+    // Store the nid and the bunble of the content type
+    $variables['nid'] = $nid;
+    $variables['bundle'] = $bundle;
+  }
+}
